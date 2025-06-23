c4c2bf376eefacc2b3f2e28c70b23343d6d73aab
trong.duong <trongduonghsht@gmail.com>
2025-06-24 00:26:33 +0700

    update main.tf round 2

--- main.tf
+++ main.tf
@@ -12,3 +12,9 @@ resource "aws_ebs_volume" "ebs-exten" {
     availability_zone = "ap-southeast-1a"
     size = 10
 }
+
+resource "aws_volume_attachment" "ebs-attach" {
+  device_name = "/dev/sdh"
+  volume_id = aws_ebs_volume.ebs-exten.id
+  instance_id = aws_instance.vm-user1.id
+}
